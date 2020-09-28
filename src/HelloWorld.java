efd0ba4ef5df6ccb649fa9033019a461058d5a6a
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-28 13:57:10 -0500

    HelloWorl Java Intro

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.print("World Hello!");
+        System.out.println("World Hello!");
+        System.out.println("World Hello!");
+        System.out.print("World Hello!");
+    }
+}
