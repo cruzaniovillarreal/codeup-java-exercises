e6b835d64196319d71a6cdf513bcc1f099f28685
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-28 15:56:04 -0500

    Basic Java Exercise C/W

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,59 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.print("World Hello!");
-        System.out.println("World Hello!");
-        System.out.println("World Hello!");
-        System.out.print("World Hello!");
+        System.out.println("Hello World!");
+
+        int myFavoriteNumber;
+        myFavoriteNumber = 32;
+        System.out.println(myFavoriteNumber);
+
+        String myString;
+        myString = "This is my string!";
+        System.out.println(myString);
+
+        long myNumber = 123L;
+        System.out.println(myNumber);
+
+        float floatNumber = 3.14F;
+        System.out.println(floatNumber);
+
+        double anotherNumber = 3.14;
+        System.out.println(anotherNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        System.out.println(o);
+
+//        Cannot cast the string "three" to an integer
+//        int three = (int) o;
+
+//        Cannot convert int to string "three"
+//        int three = (int) "three";
+
+        Object nine = 34 * 2;
+        System.out.println(nine);
+
+        x = 4;
+        x += 5;
+        System.out.println(x);
+
+        x = 3;
+        y = 4;
+        y *= x;
+        System.out.println(y);
+
+        x = 10;
+        y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
