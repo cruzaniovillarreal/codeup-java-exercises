a5950eff4e83fce31ab90b0bc98c12df816e8c6f
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-29 17:12:38 -0500

    FizzBuzz created with for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,13 +3,16 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for (int i=5; i<=15; i++) {
-            System.out.print(i + " ");
+        for (int i=1; i<100; i++) {
+            if (i%3==0 & i%5==0) {
+                System.out.print("\nFizzBuzz");
+            } else if (i%3== 0) {
+                System.out.print("\nFizz");
+            } else if (i%5== 0) {
+                System.out.print("\nBuzz");
+            } else {
+                System.out.print("\n" + i);
+            }
         }
-
-        for (double j=2; j<1000000; j = Math.pow(j, 2)) {
-            System.out.print("\n" + j);
-        }
-
     }
 }
