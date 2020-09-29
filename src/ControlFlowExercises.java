4a78f539eafe38e12c660b982dec4d20cd507749
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-29 16:45:56 -0500

    refactored while and dowhile loop into for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,20 +3,13 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
+        for (int i=5; i<=15; i++) {
             System.out.print(i + " ");
-            i++;
         }
 
-        int j = 2;
-        int x = j;
-        do {
-            j = j * (j + j);
-//            j = Math.pow(j, 2);
+        for (double j=2; j<1000000; j = Math.pow(j, 2)) {
             System.out.print("\n" + j);
-        } while (j < 1000000);
-
+        }
 
     }
 }
