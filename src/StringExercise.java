d15e969679f1107ddbb0140f1255462d4755eaad
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-01 09:33:12 -0500

    String Exercises/Escaping Characters Practice

---
+++ src/StringExercise.java
@@ -0,0 +1,12 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String newString = "We don't need no education \nWe don't need no thought control.";
+        System.out.println(newString);
+        newString = "Check \"this out!\", \"s inside of \"s!";
+        System.out.println(newString);
+        newString = "In windows, the main drive is usually C:\\";
+        System.out.println(newString);
+        newString = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
+        System.out.println(newString);
+    }
+}
