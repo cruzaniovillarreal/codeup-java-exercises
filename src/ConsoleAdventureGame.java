ffd250ea5eae56b71aa13b71f639da525bad7816
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-02 19:55:34 -0500

    Fighting Functional/Reworked

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -13,7 +13,7 @@ public class ConsoleAdventureGame {
         Scanner userLife = new Scanner(System.in);
         int life = userLife.nextInt();
         if (life > 50 || life < 1) {
-            startingLife();
+            return startingLife();
         }
         return life;
     }
@@ -80,7 +80,7 @@ public class ConsoleAdventureGame {
                 break;
             case 3:
                 System.out.println("You stumble upon a large body of water with a boat sitting on shore. There's something glowing on the island in the center of the lake... ");
-                System.out.println("Would you like to take the boat and check out the mysterious light? [y/N");
+                System.out.println("Would you like to take the boat and check out the mysterious light? [y/N]");
                 playerChoice = choice.nextLine();
                 if (playerChoice.equalsIgnoreCase("y")) {
                     enemy = "Watery "+enemy;
@@ -94,24 +94,61 @@ public class ConsoleAdventureGame {
         return enemy;
     }
 
-    public static int battleSystem(int life) {
+    public static int fighter(int playerlife, int enemyHealth) {
+        Scanner scanner1 = new Scanner(System.in);
+        if (playerlife <= 0) {
+            return playerlife;
+        } else if (enemyHealth <= 0) {
+            System.out.print("Enemy Defeated!\n");
+            return playerlife;
+        } else {
+            System.out.printf("You have %s HP. The enemy has %s HP.\n", playerlife, enemyHealth);
+            System.out.println("Attack(A) or Drink Potion(D)? [A/D]");
+            String response = scanner1.nextLine();
+            int playerHealth = playerlife;
+            if (response.equalsIgnoreCase("A")) {
+                int attackDamage = randomizer(1, 10);
+                System.out.printf("You deal %s damage.\n", attackDamage);
+                enemyHealth -= attackDamage;
+                if (enemyHealth > 0) {
+                    int retaliationDamage = randomizer(1, 15);
+                    playerHealth -= retaliationDamage;
+                    System.out.printf("The enemy retaliates dealing %s damage!\n", retaliationDamage);
+                    return fighter(playerHealth, enemyHealth);
+                } else {
+                    return fighter(playerlife, enemyHealth);
+                }
+            } else if (response.equalsIgnoreCase("D")) {
+                playerHealth += 5;
+                int retaliationDamage = randomizer(1, 15);
+                System.out.printf("You drink a potion restoring 5 health.\nThe enemy retaliates dealing %s damage!\n", retaliationDamage);
+                playerHealth -= retaliationDamage;
+                return fighter(playerHealth, enemyHealth);
+            }
+            return playerHealth;
+        }
+    }
+
+
+    public static void playerLevels(int life) {
         int playerHealth = life;
         String enemy = landscapeRandomizer();
         int enemyHealth = randomizer(1, 20);
-        System.out.println("You have "+life+" HP");
-        System.out.println("The "+enemy+" has "+enemyHealth+"HP");
-        return playerHealth;
-    }
-
-    public static void playerLevels(int life) {
-        int lifeAfterFight = battleSystem(life);
-        if (life > 0) {
+        int lifeAfterFight = fighter(playerHealth, enemyHealth);
+        Scanner scanner = new Scanner(System.in);
+        if (lifeAfterFight > 0) {
             System.out.println(potionFinder());
             System.out.print("Enter GO to continue: ");
-            Scanner scanner = new Scanner(System.in);
             String readString = scanner.nextLine();
             if (readString.equalsIgnoreCase("GO")) {
                 playerLevels(lifeAfterFight);
+            }
+        } else {
+            System.out.print("You have been killed. Would you like to play again?");
+            if (scanner.nextLine().equalsIgnoreCase("y")) {
+                gameStart();
+            } else {
+                System.out.print("Game Over. Seriously.");
             }
         }
 
