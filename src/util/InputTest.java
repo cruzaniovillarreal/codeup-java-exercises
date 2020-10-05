69398ef5d5693e404aa6fdc1d6b37a73c0939a18
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-05 16:09:51 -0500

    Exercise 2 of OOP completed

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class InputTest {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Input newScanner = new Input();
-        newScanner.scanner = new Scanner(System.in);
+        System.out.println(newScanner.getString());
+        System.out.println(newScanner.yesNo());
+        System.out.println(newScanner.getInt(1, 10));
+        System.out.println(newScanner.getInt());
+        System.out.println(newScanner.getDouble(10.00, 20.00));
+        System.out.println(newScanner.getDouble());
     }
 
 }
