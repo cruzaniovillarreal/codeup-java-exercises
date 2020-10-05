1b69571b6ae3d814f07031e1324a482f230cfc58
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-05 16:50:20 -0500

    DishTools and Test added/modified

--- src/DishTest.java
+++ src/DishTest.java
@@ -5,6 +5,9 @@ public class DishTest {
         dish1.nameOfDish = "SuperAwesomeDishOfAwesomeness";
         dish1.wouldRecommend = true;
         dish1.printSummary();
+        DishTools.shoutDishName(dish1);
+        DishTools.analyzeDishCost(dish1);
+        DishTools.flipRecommendation(dish1);
 
     }
 }
--- src/DishTools.java
+++ src/DishTools.java
@@ -1,2 +1,23 @@
+import java.sql.SQLOutput;
+
 public class DishTools {
+
+    public static final int AVERAGE_COST_OF_DISH_IN_CENT = 13000;
+
+    public static void shoutDishName(Dish dish) {
+        System.out.print(dish.nameOfDish.toUpperCase());
+    }
+
+    public static void analyzeDishCost(Dish dish) {
+        if (dish.costInCents > AVERAGE_COST_OF_DISH_IN_CENT) {
+            System.out.println("\nMore expensive than average");
+        } else {
+            System.out.println("\nLess expensive than average");
+        }
+    }
+
+    public static void flipRecommendation(Dish dish) {
+        dish.wouldRecommend = !dish.wouldRecommend;
+        System.out.println(dish.wouldRecommend);
+    }
 }
