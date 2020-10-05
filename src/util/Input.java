9ef2e454a15f36f6192c39c2bc25a44fe13fc8d7
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-05 13:51:19 -0500

    OOP Intro

---
+++ src/util/Input.java
@@ -0,0 +1,28 @@
+package util;
+
+public class Input {
+    private String scanner;
+
+    public String getString() {
+
+    }
+
+    public boolean yesNo() {
+
+    }
+
+    public int getInt(int min, int max) {
+
+    }
+
+    public int getInt() {
+
+    }
+
+    public double getDouble(double min, double max) {
+
+    }
+    public double getDouble() {
+
+    }
+}
