503d62a3174bab3f349f3f71f54bc20142d2adbc
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-08 11:42:12 -0500

    Board generator created, working on turn loop

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,21 +1,33 @@
 import util.Input;
 
+import java.util.Arrays;
+
 public class TicTacToe {
 
     public static void game() {
         System.out.println("Welcome to Tic-Tac-Toe!");
         System.out.println("Response Structure: (rowNumber)(columnLetter) / Ex. 1a, 3c");
-        gameBoard();
-        String player1 = new Input().getString("\nPlayer 1: ");
+        String[][] startBoard = startBoard();
+        turn1(startBoard);
+
     }
 
-    public static String[][] gameBoard() {
+    public static String[][] turn1(String[][] startBoard) {
+        String player1 = new Input().getString("\nPlayer 1: ");
+        String[] p1Breakdown = player1.split("");
+        int row = (Integer.parseInt(p1Breakdown[0]));
+        int column = positionToNum(p1Breakdown[1]);
+        String[][] newBoard = newBoard(startBoard, row, column);
+        return newBoard;
+    }
+
+    public static String[][] startBoard() {
         String[][] matrix = {
+                {"   A   B   C  "},
                 {"1", "---", "---", "---"},
-                {"2","---", "---", "---"},
-                {"3","---", "---", "---"},
+                {"2", "---", "---", "---"},
+                {"3", "---", "---", "---"},
         };
-        System.out.println("    A   B   C  ");
         for (String[] row : matrix) {
             System.out.print(" ");
             for (String n : row) {
@@ -26,8 +38,17 @@ public class TicTacToe {
         return matrix;
     }
 
-    public static String[] usableLocation(String[] userInput) {
-
+    public static String[][] newBoard(String[][] oldMatrix, int row, int column) {
+        String[][] newMatrix = Arrays.copyOf(oldMatrix, oldMatrix.length);
+        newMatrix[row][column] = "-X-";
+        for (String[] eachRow : newMatrix) {
+            System.out.print(" ");
+            for (String n : eachRow) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+        return newMatrix;
     }
 
     public static int positionToNum(String position) {
