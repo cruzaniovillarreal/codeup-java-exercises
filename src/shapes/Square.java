9a72ffb530ba90f2dd1fab9a1af0defd645908f8
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-09 11:14:38 -0500

    Exercise setup

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,9 @@
 package shapes;
 import shapes.Rectangle;
 
-class Square extends Rectangle {
-    public Square(int side) {
-        super(side, side);
-    }
+public class Square extends Rectangle {
 
-    public int getPerimeter() {
-        System.out.println("Using Square Perimeter Override!");
-        return 4 * this.length;
-    }
 
-    public int getArea() {
-        System.out.println("Using Square Area Override!");
-        return (int)Math.pow(this.length, 2);
-    }
 }
 
 
