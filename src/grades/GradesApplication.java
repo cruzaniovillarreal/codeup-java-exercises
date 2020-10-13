530bbf73622bb58a2469a898a255796dd1708916
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-13 15:25:16 -0500

    Collection exercise 4&5 completed

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -11,21 +11,33 @@ public class GradesApplication {
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
         Student student1 = new Student("Jack");
-        student1.addGrade(85);
-        student1.addGrade(89);
-        student1.addGrade(92);
+        student1.recordAttendance("2017-10-17", "P");
+        student1.recordAttendance("2017-10-18", "A");
+        student1.recordAttendance("2017-10-19", "P");
+        student1.addGrade(90);
+        student1.addGrade(90);
+        student1.addGrade(90);
         Student student2 = new Student("Eddy");
-        student2.addGrade(93);
-        student2.addGrade(97);
+        student2.recordAttendance("2017-10-17", "A");
+        student2.recordAttendance("2017-10-18", "P");
+        student2.recordAttendance("2017-10-19", "A");
+        student2.addGrade(60);
+        student2.addGrade(90);
         student2.addGrade(100);
         Student student3 = new Student("James");
-        student3.addGrade(85);
-        student3.addGrade(74);
+        student3.recordAttendance("2017-10-17", "P");
+        student3.recordAttendance("2017-10-18", "P");
+        student3.recordAttendance("2017-10-19", "P");
+        student3.addGrade(70);
         student3.addGrade(80);
+        student3.addGrade(90);
         Student student4 = new Student("Max");
-        student4.addGrade(67);
-        student4.addGrade(73);
-        student4.addGrade(77);
+        student4.recordAttendance("2017-10-17", "A");
+        student4.recordAttendance("2017-10-18", "A");
+        student4.recordAttendance("2017-10-19", "A");
+        student4.addGrade(80);
+        student4.addGrade(50);
+        student4.addGrade(60);
         students.put("JackyBoy", student1);
         students.put("EddyBoy", student2);
         students.put("JamesyBoy", student3);
@@ -40,20 +52,35 @@ public class GradesApplication {
             for (String student : students.keySet()) {
                 System.out.print("|"+student+"|"+" ");
             }
-            String userInput = new Input().getString("\n\nEnter username to expand: ");
-            if (!students.containsKey(userInput)) {
+            String userInput = new Input().getString("\n\nEnter username to view specific student information or type 'class' for summary: ");
+            if (userInput.equalsIgnoreCase("class")) {
+                ArrayList<Integer> allGrades = new ArrayList<>();
+                int classSize = 0;
+                int classTotal = 0;
+                for (String student : students.keySet()) {
+                    classSize += 1;
+//                    classTotal += students.get(student).getGradeAverage();
+                    allGrades.addAll(students.get(student).getGrades());
+                    System.out.println(students.get(student).getName()+", "+student+", "+students.get(student).getGrades()+", "+students.get(student).getGradeAverage());
+                }
+                for (Integer grade : allGrades) {
+                    classTotal += grade;
+                }
+                System.out.println("Class Size: "+classSize);
+                System.out.println("Class Average: "+classTotal/allGrades.size());
+            } else if (!students.containsKey(userInput)) {
                 System.out.println("No users exist with the entered username.");
             } else {
                 System.out.println("Name: "+students.get(userInput).getName());
                 System.out.println("Grades: "+students.get(userInput).getGrades());
                 System.out.println("Grade Average: "+students.get(userInput).getGradeAverage());
+                System.out.printf("Attendance Percentage: %.2f", students.get(userInput).attendancePercentage());
+                System.out.println("\nAbsent Dates: "+students.get(userInput).absentDates());
             }
-            String userContinue = new Input().getString("Would you like to continue?");
+            String userContinue = new Input().getString("\nWould you like to continue? [y/N]");
             if (userContinue.equalsIgnoreCase("y")) {
                 toContinue = true;
             }
         } while (toContinue);
-
-
     }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,41 +1,70 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Student {
     private String name;
-    private ArrayList<Integer> grade;
+    private ArrayList<Integer> grades;
+    private HashMap<String, String> attendance = new HashMap<>();
 
 
     public Student(String name) {
         this.name = name;
-        this.grade = new ArrayList<>();
+        this.grades = new ArrayList<>();
     }
 
-    // returns the student's name
     public String getName() {
         return this.name;
     }
 
-    // adds the given grade to the grades property
     public void addGrade(int grade) {
-        (this.grade).add(grade);
+        (this.grades).add(grade);
     }
 
-    // returns the average of the students grades
     public double getGradeAverage() {
         double total = 0;
-        for (Integer grade : this.grade) {
+        for (Integer grade : this.grades) {
             total += grade;
         }
-        return total / this.grade.size();
+        return total / this.grades.size();
     }
 
     public ArrayList<Integer> getGrades() {
-        return this.grade;
+        return this.grades;
 //        for (Integer grade : this.grade) {
 //            System.out.println(grade);
 //        }
+    }
+
+    public void recordAttendance(String date, String value) {
+        if (value.equalsIgnoreCase("P") || value.equalsIgnoreCase("A")) {
+            this.attendance.put(date, value);
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
+
+    public float attendancePercentage() {
+        float total = 0;
+        float absent = 0;
+        for (String date : this.attendance.keySet()) {
+            total += 1;
+            if (this.attendance.get(date).equalsIgnoreCase("A")) {
+                absent += 1;
+            }
+        }
+        return (total-absent)/total;
+    }
+
+    public ArrayList<String> absentDates() {
+        ArrayList<String> dates = new ArrayList<>();
+        for (String date : this.attendance.keySet()) {
+            if (this.attendance.get(date).equalsIgnoreCase("A")) {
+                dates.add(date);
+            }
+        }
+        return dates;
     }
 
     public static void main(String[] args) {
